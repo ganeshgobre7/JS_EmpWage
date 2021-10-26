9043499c53cafabd6039cec33a2d83b1e44f4862
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-26 14:33:06 +0530

    Welcome message added

---
+++ EmployeeWage/Empwage.js
@@ -0,0 +1,10 @@
+let empCheck = Math.floor(Math.random() * 2 );
+if (empCheck == 0)
+    {
+       console.log("Employee is PRESENT");
+    }
+else
+    {
+        console.log("Employee is ABSENT");
+    }
+    console.log(empCheck);
